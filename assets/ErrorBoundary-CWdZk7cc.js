2ca1a6942a3e08f45aa3895c505eb89030f60d90
R-U-Bharti <kumarrubhart.rtcit@gmail.com>
2025-01-20 22:56:38 +0530

    Updates

--- assets/ErrorBoundary-CWdZk7cc.js
+++ assets/ErrorBoundary-CWdZk7cc.js
@@ -1,4 +1,4 @@
-import{b as a,j as e}from"./index-CPPO-F3U.js";import{c as n}from"./createLucideIcon-DM0vRC76.js";/**
+import{b as a,j as e}from"./index-BpdbWUoU.js";import{c as n}from"./createLucideIcon-BlMob86V.js";/**
  * @license lucide-react v0.473.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/createLucideIcon-BlMob86V.js
+++ assets/createLucideIcon-BlMob86V.js
@@ -1,4 +1,4 @@
-import{r as a}from"./index-CPPO-F3U.js";/**
+import{r as a}from"./index-BpdbWUoU.js";/**
  * @license lucide-react v0.473.0 - ISC
  *
  * This source code is licensed under the ISC license.
