60cf30b084f11f59a143be7ace8ae8c8283f103f
R-U-Bharti <kumarrubhart.rtcit@gmail.com>
2025-01-24 13:50:01 +0530

    Updates

--- assets/ErrorBoundary-CjgBohRS.js
+++ assets/ErrorBoundary-CjgBohRS.js
@@ -1,4 +1,4 @@
-import{b as a,j as e}from"./index-D55Swj75.js";import{c as n}from"./createLucideIcon-8kpPDEv0.js";/**
+import{b as a,j as e}from"./index-5yK8KDe3.js";import{c as n}from"./createLucideIcon-DovhIMT_.js";/**
  * @license lucide-react v0.473.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/createLucideIcon-DovhIMT_.js
+++ assets/createLucideIcon-DovhIMT_.js
@@ -1,4 +1,4 @@
-import{r as a}from"./index-D55Swj75.js";/**
+import{r as a}from"./index-5yK8KDe3.js";/**
  * @license lucide-react v0.473.0 - ISC
  *
  * This source code is licensed under the ISC license.
